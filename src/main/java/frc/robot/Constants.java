e31247cced6c2eabbd1860466fbfd4c83970d30f
dt800298 <dt800298@gmail.com>
2020-01-05 12:12:45 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class Constants{
+    //change in the future for specificity 
+    public static final int TALON_RIGHTFRONT = -1;
+    public static final int TALON_LEFTFRONT = -1;
+    public static final int TALON_LEFTBACK = -1;
+    public static final int TALON_RIGHTBACK = -1;
+
+
+    
+
+}
