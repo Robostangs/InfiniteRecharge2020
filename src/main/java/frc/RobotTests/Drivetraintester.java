75ec0cb0b837b1658c52935d9f952d9075c485a0
The-Jack-Smith <35631054+The-Jack-Smith@users.noreply.github.com>
2020-02-04 19:44:33 -0500

    Updated to use MotorController

--- src/main/java/frc/RobotTests/Drivetraintester.java
+++ src/main/java/frc/RobotTests/Drivetraintester.java
@@ -13,11 +13,11 @@ public class Drivetraintester {
 
     private static Drivetrain dt = new Drivetrain();
 
-    public static void dtTester(CANSparkMax leftFront, CANSparkMax leftMiddle, CANSparkMax leftBack, CANSparkMax rightFront, CANSparkMax rightMiddle, CANSparkMax rightBack)
+    public static void dtTester(MotorController leftFront, MotorController leftMiddle, MotorController leftBack, MotorController rightFront, MotorController rightMiddle, MotorController rightBack)
     {
 
         
-        List<CANSparkMax> motors = new ArrayList<CANSparkMax>();
+        List<MotorController> motors = new ArrayList<MotorController>();
         boolean allMotorsFunctional = true;
 
         motors.add(leftFront);
@@ -32,11 +32,11 @@ public class Drivetraintester {
         
         
         
-        for(CANSparkMax motor : motors){
+        for(MotorController motor : motors){
             if(Constants.MOTOR_ACCEPTED_MIN_CURRENT >= motor.getOutputCurrent() || motor.getOutputCurrent() >= Constants.MOTOR_ACCEPTED_MAX_CURRENT)
             {   
                 System.out.println("!!! CURRENT ERROR !!!");
-                System.out.println("CANSparkMax: " + motor + " is currently outputting at " + motor.getOutputCurrent() + " Amps!");
+                System.out.println("MotorController: " + motor.getName() + " is currently outputting at " + motor.getOutputCurrent() + " Amps!");
                 System.out.println("...Its expected output is around " + (Constants.MOTOR_ACCEPTED_MIN_CURRENT + Constants.MOTOR_ACCEPTED_MAX_CURRENT)/2 + " Amps!");
                 System.out.println();
                 allMotorsFunctional = false;
@@ -45,7 +45,7 @@ public class Drivetraintester {
             if(Constants.MOTOR_ACCEPTED_MIN_VELOCITY >= motor.getEncoder().getVelocity() || motor.getEncoder().getVelocity() >= Constants.MOTOR_ACCEPTED_MAX_VELOCITY)
             {   
                 System.out.println("!!! VELOCITY ERROR !!!");
-                System.out.println("CANSparkMax: " + motor + " is currently outputting at " + motor.getEncoder().getVelocity() + " RPM!");
+                System.out.println("MotorController: " + motor.getName() + " is currently outputting at " + motor.getEncoder().getVelocity() + " RPM!");
                 System.out.println("...Its expected output is around " + (Constants.MOTOR_ACCEPTED_MIN_VELOCITY + Constants.MOTOR_ACCEPTED_MAX_VELOCITY)/2 + " RPM!");
                 System.out.println();
                 allMotorsFunctional = false;
